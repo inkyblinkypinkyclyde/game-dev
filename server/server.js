7cad49dd2738c8a3eb191d7200e981d477bf073a
Calum Cook <calumcook@Calums-Air.broadband>
2022-07-15 16:35:04 +0100

    api and server req, res working

--- server/server.js
+++ server/server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
-
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+
+app.use(express.json());
+app.use(cors());
+
+MongoClient.connect('mongodb://127.0.0.1:27017',
+{useUnifiedTopology: true})
+.then((client) => {
+  const db = client.db('grid');
+  const tilesCollection = db.collection('tiles');
+  const gridRouter = createRouter(tilesCollection);
+  app.use('/api/tiles', gridRouter);
+})
+
+.catch(console.error)
 
 app.get('/', function require(req, res) {
   res.json({message: "This is working!"});
