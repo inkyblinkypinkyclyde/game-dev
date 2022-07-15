761e1ac4b6659342d0c6cd3a1e4ea9dd7f6603cf
inkyblinkypinkyclyde <42178384+inkyblinkypinkyclyde@users.noreply.github.com>
2022-07-15 22:26:28 +0100

    fixed a typo (gotta up those commits!)

--- client/src/components/BattleshipGrid.js
+++ client/src/components/BattleshipGrid.js
@@ -17,38 +17,6 @@ const BattleshipGrid = ({
     gamePhase
 }) => {
 
-    // const cellGrid = () => {
-    //     if (playerGrid === true) {
-    //         playerCells.map((cell1) => {
-    //             return (
-    //                 <BattleshipCell
-    //                     playerGrid={playerGrid}
-    //                     cell1={cell1}
-    //                     key={cell1.number}
-    //                     shotAt={cell1.shotAt}
-    //                     shipPresent={cell1.shipPresent}
-    //                     onCellClickAttack={onCellClickAttack}
-    //                     onCellClickPlace={onCellClickPlace}
-    //                 />
-    //             )
-    //         })
-    //     } else {
-    //         opponentCells.map((cell2) => {
-    //             return (
-    //                 <BattleshipCell
-    //                     playerGrid={playerGrid}
-    //                     cell2={cell2}
-    //                     key={cell2.number}
-    //                     shotAt={cell2.shotAt}
-    //                     shipPresent={cell2.shipPresent}
-    //                     onCellClickAttack={onCellClickAttack}
-    //                     onCellClickPlace={onCellClickPlace}
-    //                 />
-    //             )
-    //         })
-    //     }
-    // }
-
     const cellGridPlayer = playerCells.map((cell) => {
         return (
             <BattleshipCell
