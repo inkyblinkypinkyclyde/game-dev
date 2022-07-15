537d8025741ae9681fd5af8da79fdc951fc24500
Jack Slater <JSLATE201@caledonian.ac.uk>
2022-07-15 15:04:11 +0100

    updated Nav Bar layout

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,39 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+import styled from "styled-components"
 
+const NavList =styled.ul`
+display:flex;
+background: #ffffff;
+justify-content: space-evenly;
+list-style: none;
+`
+
+const Heading = styled.h1`
+display:flex;
+justify-content: center;
+`
+
+const StyledLink = styled(Link)`
+text-decoration: none;
+color: #054569;
+`
 const NavBar = () => {
     return (
-        <>
-            <h1>React Games Hub</h1>
-            <a href=''>Home</a>
-            <a href=''>A link</a>
-            <a href=''>Another link</a>
+        <><Heading>React'R'cade</Heading>
+            <NavList>
+                <li>
+                    <StyledLink to="/">Home</StyledLink>
+                </li>
+            
+                <li>
+                    <StyledLink to="/battleships">Battleships</StyledLink>
+                </li>
+
+                <li>
+                    <StyledLink to="/about">About</StyledLink>
+                </li>
+            </NavList> 
         </>
     )
 }
